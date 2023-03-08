091298370a1088e0466ac74c82420c8f943e9760
vf98 <vf98@AR4-Asistent.ftn.ns>
2023-03-08 15:45:37 +0100

    Druge vezbe 8.3.2023.

---
+++ currency-exchange/src/main/java/currencyExchange/CurrencyExchangeApplication.java
@@ -0,0 +1,13 @@
+package currencyExchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeApplication.class, args);
+	}
+
+}
