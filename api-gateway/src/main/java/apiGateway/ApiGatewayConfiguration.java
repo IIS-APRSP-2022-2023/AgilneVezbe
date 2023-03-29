bab0b309c1816f496fbf26d92bb32e61a3c72443
vf98 <vf98@AR4-Asistent.ftn.ns>
2023-03-29 14:22:21 +0200

    Pete vezbe API-Gateway

---
+++ api-gateway/src/main/java/apiGateway/ApiGatewayConfiguration.java
@@ -0,0 +1,28 @@
+package apiGateway;
+
+import java.util.function.Function;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+	
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		
+		return builder
+				.routes()
+				.route(p -> p.path("/@Depaiva1997").uri("http://youtube.com"))
+				.route(p -> p.path("/currency-exchange/**").uri("lb://currency-exchange"))
+				.build();
+	}
+	
+	//http://localhost:8765/currency-exchange/from/USD/to/RSD
+}
