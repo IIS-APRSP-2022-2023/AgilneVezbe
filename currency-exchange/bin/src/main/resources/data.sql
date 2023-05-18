77089809e70e20090c3e4e20e73404a26ed12d17
VladimirFabri98 <vfficial98@gmail.com>
2023-05-18 21:32:00 +0200

    Vezbe 10 - Kreiranje docker image-a putem pom.xml i dockerfile-a

---
+++ currency-exchange/bin/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10001,'EUR','RSD',117);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10002,'USD','RSD',120);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10003,'GBP','RSD',140);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10004,'CHF','RSD',115);
+
+insert into currency_exchange(id,currency_from,currency_to,conversion_multiple)
+values(10005,'RUB','RSD',8);
